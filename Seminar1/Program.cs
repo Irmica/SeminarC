81e25dd3f0e5f5ba5b8ce11662326aa2ce8b0d39
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-24 10:42:55 +0300

    Добавлена памятка по типам ввода и конвртация из типа строки в целые

---
+++ Seminar1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Console.WriteLine("Good morning!");
+// int num1 = 5;
+// int num2 = 7;
+// Console.WriteLine("My numbers are " + num1 + " and " + num2);
+// Console.WriteLine($"My numbers are {num1} and {num2}");
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
