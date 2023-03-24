b4e2eb80e4da2e09dbbdb835c1cf82d4e1942701
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-24 11:37:31 +0300

    Добавлена задача 1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -5,3 +5,26 @@
 // Console.WriteLine($"My numbers are {num1} and {num2}");
 // Console.Write("Input a number: ");
 // int num = Convert.ToInt32(Console.ReadLine());
+
+// Задача 1.
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad2 = num2 * num2;
+
+// if (num1 == quad2)
+// {
+//     Console.WriteLine("Yes!");
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+
+// Задача 2.
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
