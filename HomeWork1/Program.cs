c5fec055678dcdfd02f6bf54fec6f284800e11e3
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-24 14:15:51 +0300

    Добавлена 1 задача

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 > num2)
+// {
+//     Console.Write($"{num1} - большее число, {num2} - меньшее число");
+// }
+// else Console.Write($"{num2} - большее число, {num1} - меньшее число");
