886705284235e10c7d7ec4be9c98a82ece0e8226
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-30 01:48:52 +0300

    Add Seminar2, Задача 2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,16 +1,42 @@
 ﻿// Задача 1. Напишите программу, которая выводит случайное трёхзначное
 //  число и удаляет вторую цифру этого числа.
 
-int CutNumber(int num )
-{
-    int h = num /100;
-    int u = num % 10;
+// int CutNumber(int num )
+// {
+//     int h = num /100;
+//     int u = num % 10;
     
-    return h*10 + u;
-}
+//     return h*10 + u;
+// }
 
-int randNumber = new Random().Next(100, 1000);
-int result = CutNumber(randNumber);
-Console.WriteLine(randNumber);
-Console.WriteLine(result);
+// int randNumber = new Random().Next(100, 1000);
+// int result = CutNumber(randNumber);
+// Console.WriteLine(randNumber);
+// Console.WriteLine(result);
+//-----------------------------------------------------------------------------
+// Задача 2. Напишите программу, которая выводит случайное 
+// число из отрезка [10, 99] и показывает наибольшую цифру числа.
 
+// int MaxDigit(int num)
+// {
+//     int d1 = num % 10;
+//     int d2 = num/10;
+//     int max = d1;
+//     if (d2 > max) max = d2;
+//     return max;
+// }
+// int num = new Random().Next(10, 100);
+// int result = MaxDigit(num);
+// Console.WriteLine(num);
+// Console.WriteLine(result);
+//---------------------------------------------------------------------------------
+// Задача 3. Напишите программу, которая принимает на 
+// вход число и проверяет, кратно ли оно одновременно a и b.
+
+
+
+
+
+
+// Задача 4. Напишите программу, которая принимает на вход два числа 
+// и проверяет, является ли одно число квадратом другого.
