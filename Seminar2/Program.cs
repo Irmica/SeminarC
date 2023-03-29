ca6087baacbe291857418a3db449365f118bda56
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-30 02:28:36 +0300

    Добавлены задачи 2, 3, 4

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -5,7 +5,7 @@
 // {
 //     int h = num /100;
 //     int u = num % 10;
-    
+
 //     return h*10 + u;
 // }
 
@@ -29,14 +29,39 @@
 // int result = MaxDigit(num);
 // Console.WriteLine(num);
 // Console.WriteLine(result);
-//---------------------------------------------------------------------------------
+//---------------------------------------------------------------
 // Задача 3. Напишите программу, которая принимает на 
 // вход число и проверяет, кратно ли оно одновременно a и b.
 
-
-
-
-
+// bool Multiplicity(int n, int a, int b)
+// {
+//     bool res = false;
+//     if (n % a == 0 && n % b == 0)
+//         res = true;
+//     return res;
+// }
+// Console.Write("Введите проверяемое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первый делитель: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второй делитель: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Multiplicity(n, a, b));
+//-------------------------------------------------------------------
 
 // Задача 4. Напишите программу, которая принимает на вход два числа 
 // и проверяет, является ли одно число квадратом другого.
+
+bool Square(int a, int b)
+{
+    bool res = false;
+    if ((a*a == b) || (b*b == a))
+    res = true;
+    return res;
+}
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Square(a, b));
