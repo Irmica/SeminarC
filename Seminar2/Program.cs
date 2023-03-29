f62181093a2a14b2e7032c86a844c3bf17a1e1ef
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-30 01:09:41 +0300

    Add Seminar2, Задача 1, методы

---
+++ Seminar2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1. Напишите программу, которая выводит случайное трёхзначное
+//  число и удаляет вторую цифру этого числа.
+
+int CutNumber(int num )
+{
+    int h = num /100;
+    int u = num % 10;
+    
+    return h*10 + u;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int result = CutNumber(randNumber);
+Console.WriteLine(randNumber);
+Console.WriteLine(result);
+
