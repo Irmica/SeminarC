d8b3e2fdf7a9f28c5beb94b496cf580881b70d4a
Irmica <128592858+Irmica@users.noreply.github.com>
2023-03-30 03:23:43 +0300

    Добавлена задача 15

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -10,29 +10,38 @@
 // Console.WriteLine("Введите трехзначное число: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine(SecondDigit(n));
+
 //---------------------------------------------------------------------
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-int ThirdDigit(int n)
-{
-    if (n < 100) return -1;
-    else
-    {
-        while ((n/1000)>1)
-    {
-        n = n/10;
-        }
-    int d3  = n % 10;
-    return d3;
-    }
-}
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (ThirdDigit(num) == -1) Console.WriteLine("Третьей цифры нет");
-else Console.WriteLine(ThirdDigit(num));
+// int ThirdDigit(int n)
+// {
+//     if (n < 100) return -1;
+//     else
+//     {
+//         while ((n/1000)>1)
+//     {
+//         n = n/10;
+//         }
+//     int d3  = n % 10;
+//     return d3;
+//     }
+// }
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (ThirdDigit(num) == -1) Console.WriteLine("Третьей цифры нет");
+// else Console.WriteLine(ThirdDigit(num));
 
-
+//------------------------------------------------------------------------
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
 //  и проверяет, является ли этот день выходным.
 
+bool Weekend (int d)
+{
+    if (d == 6 || d ==7) return true;
+    else return false;
+}
+Console.WriteLine("Введите цифру дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Weekend(num));
 
