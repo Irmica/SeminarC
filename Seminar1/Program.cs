e7f3ef4408aa5257265ed5e6a1e2d7418092f542
Irmica <128592858+Irmica@users.noreply.github.com>
2023-04-03 09:49:07 +0300

    Правки

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -28,12 +28,13 @@
 // Задача 2.
 // Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
 
-Console.Write("Input positivi number: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input positivi number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-int current = -num;
-while (current <= num)
-{
-Console.Write(current + " ");
-current++;
-}
+// int current = -num;
+// while (current <= num)
+// {
+// Console.Write(current + " ");
+// current++;
+// }
+
