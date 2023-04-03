dbb110480ce35f360e08f66bf2c88b83b43c8a0f
Irmica <128592858+Irmica@users.noreply.github.com>
2023-04-03 10:19:56 +0300

    Добавлен 4 семинар 1 задача

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+int GetSum (int a)
+{
+    int sum = 0;
+for (int i = 1; i <= a; i += 1)
+        sum = sum + i;
+return sum;
+}
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetSum(a)); 
