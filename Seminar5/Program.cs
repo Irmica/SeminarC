8dc1027dc083f3906e678019a7df5a963bbc32ba
Irmica <128592858+Irmica@users.noreply.github.com>
2023-04-06 13:51:53 +0300

    Я хз что произошло)

---
+++ Seminar5/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных элементов массива.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i+=1)
+       array[i] = new Random().Next(minValue, maxValue +1);
+    return array;   
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length;i +=1)
+     Console.Write(array[i] + " ");
+Console.WriteLine();
+}
+
+int GetNegativeNumbers(int[] array)
+{
+    int sum = 0;
+    for (i = 0; i < array.Length; i += 1)
+    {
+        if (array[i] < 0)
+            sum += array[i];
+    }
+    return sum;
+}
+// Напишите программу замена элементов массива: положительные элементы замените 
+// на соответствующие отрицательные, и наоборот.
+
+
+//Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+//Задайте одномерный массив из m случайных чисел. Найдите количество элементов массива,
+// значения которых лежат в отрезке [a,b].
