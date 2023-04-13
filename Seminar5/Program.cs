c03b5fdccf6150f6c12345d09b5660c9548c46ff
Irmica <128592858+Irmica@users.noreply.github.com>
2023-04-13 09:14:51 +0300

    Из кода сделан комментарий

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,30 +1,30 @@
 ﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
 // Найдите сумму отрицательных элементов массива.
 
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-    int[] array = new int[size];
-    for(int i = 0; i < size; i+=1)
-       array[i] = new Random().Next(minValue, maxValue +1);
-    return array;   
-}
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length;i +=1)
-     Console.Write(array[i] + " ");
-Console.WriteLine();
-}
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i+=1)
+//        array[i] = new Random().Next(minValue, maxValue +1);
+//     return array;   
+// }
+// void ShowArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length;i +=1)
+//      Console.Write(array[i] + " ");
+// Console.WriteLine();
+// }
 
-int GetNegativeNumbers(int[] array)
-{
-    int sum = 0;
-    for (int i = 0; i < array.Length; i += 1)
-    {
-        if (array[i] < 0)
-            sum += array[i];
-    }
-    return sum;
-}
+// int GetNegativeNumbers(int[] array)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < array.Length; i += 1)
+//     {
+//         if (array[i] < 0)
+//             sum += array[i];
+//     }
+//     return sum;
+// }
 // Напишите программу замена элементов массива: положительные элементы замените 
 // на соответствующие отрицательные, и наоборот.
 
